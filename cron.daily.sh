9d778c472c782e6e2d7f39f79475015916b39d21
Jorge Aparicio <japaricious@gmail.com>
2016-11-14 13:41:48 -0500

    tweak cron script to run on server

--- cron.daily.sh
+++ cron.daily.sh
@@ -4,10 +4,19 @@ set -ex
 
 ( cd rustfmt && git pull )
 
-git diff-index --quiet HEAD -- || (
+test -z "$(git status -s)" || (
+    git config user.name "Jorge Aparicio"
+    git config user.email "japaricious@gmail.com"
+
     git add rustfmt
     git commit -m "update the rustfmt submodule"
-    git push
+
+    set +x
+    git push https://$(cat ~/.gh_token)@github.com/japaric/rustfmt-bin.git && echo OK
+    set -x
+
     git tag -a v$(grep version rustfmt/Cargo.toml | cut -d'"' -f2)-$(date +"%Y%m%d") -m "deploy"
-    git push --tags
+
+    set +x
+    git push https://$(cat ~/.gh_token)@github.com/japaric/rustfmt-bin.git --tags && echo OK
 )
