ae23bd2b40c679178f513dd001b0a8b3ecb5977d
Jorge Aparicio <japaricious@gmail.com>
2016-11-14 11:26:03 -0500

    add a script for the daily cron job

---
+++ cron.daily.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+( cd rustfmt && git pull )
+
+git diff-index --quiet HEAD -- || (
+    git add rustfmt
+    git commit -m "update the rustfmt submodule"
+    git push
+    git tag -a v$(grep version rustfmt/Cargo.toml | cut -d'"' -f2)-$(date +"%Y%m%d") "deploy"
+    git push --tags
+)
