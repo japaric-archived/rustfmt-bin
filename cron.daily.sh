0dd35420d94c1e8dbf6c45071a7831282b3b7948
Jorge Aparicio <japaricious@gmail.com>
2016-11-14 11:28:02 -0500

    cron: fix the git tag command

--- cron.daily.sh
+++ cron.daily.sh
@@ -8,6 +8,6 @@ git diff-index --quiet HEAD -- || (
     git add rustfmt
     git commit -m "update the rustfmt submodule"
     git push
-    git tag -a v$(grep version rustfmt/Cargo.toml | cut -d'"' -f2)-$(date +"%Y%m%d") "deploy"
+    git tag -a v$(grep version rustfmt/Cargo.toml | cut -d'"' -f2)-$(date +"%Y%m%d") -m "deploy"
     git push --tags
 )
